b0cd577e714df0f4bb52ca4cef72411b2189ef88
usman64 <usmantariq65@outlook.com>
2020-06-01 02:08:53 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <h1>BootCamp 2020: Assignment 2</h1>
-      <h3 style={{ color: 'blue' }}>Muhammad Usman Tariq</h3>
+      <h3>Muhammad Usman Tariq</h3>
       <quote>
         CI/CD implemmented and automated via Github Actions workflow | Deployed
         on surge
